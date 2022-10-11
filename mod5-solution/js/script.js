02152d40cf867b7213489f0ca7b3e95585d44378
Ahkins <michaelahkins@yahoo.com>
2022-10-11 09:34:11 +0100

    Update script.js

--- mod5-solution/js/script.js
+++ mod5-solution/js/script.js
@@ -11,54 +11,54 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 
 (function (global) {
 
-  var dc = {};
+var dc = {};
 
-  var homeHtmlUrl = "snippets/home-snippet.html";
-  var allCategoriesUrl =
-    "https://davids-restaurant.herokuapp.com/categories.json";
-  var categoriesTitleHtml = "snippets/categories-title-snippet.html";
-  var categoryHtml = "snippets/category-snippet.html";
-  var menuItemsUrl =
-    "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
-  var menuItemsTitleHtml = "snippets/menu-items-title.html";
-  var menuItemHtml = "snippets/menu-item.html";
+var homeHtmlUrl = "snippets/home-snippet.html";
+var allCategoriesUrl =
+  "https://davids-restaurant.herokuapp.com/categories.json";
+var categoriesTitleHtml = "snippets/categories-title-snippet.html";
+var categoryHtml = "snippets/category-snippet.html";
+var menuItemsUrl =
+  "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
+var menuItemsTitleHtml = "snippets/menu-items-title.html";
+var menuItemHtml = "snippets/menu-item.html";
 
-  // Convenience function for inserting innerHTML for 'select'
-  var insertHtml = function (selector, html) {
-    var targetElem = document.querySelector(selector);
-    targetElem.innerHTML = html;
-  };
+// Convenience function for inserting innerHTML for 'select'
+var insertHtml = function (selector, html) {
+  var targetElem = document.querySelector(selector);
+  targetElem.innerHTML = html;
+};
 
-  // Show loading icon inside element identified by 'selector'.
-  var showLoading = function (selector) {
-    var html = "<div class='text-center'>";
-    html += "<img src='images/ajax-loader.gif'></div>";
-    insertHtml(selector, html);
-  };
+// Show loading icon inside element identified by 'selector'.
+var showLoading = function (selector) {
+  var html = "<div class='text-center'>";
+  html += "<img src='images/ajax-loader.gif'></div>";
+  insertHtml(selector, html);
+};
 
-  // Return substitute of '{{propName}}'
-  // with propValue in given 'string'
-  var insertProperty = function (string, propName, propValue) {
-    var propToReplace = "{{" + propName + "}}";
-    string = string
-      .replace(new RegExp(propToReplace, "g"), propValue);
-    return string;
-  };
+// Return substitute of '{{propName}}'
+// with propValue in given 'string'
+var insertProperty = function (string, propName, propValue) {
+  var propToReplace = "{{" + propName + "}}";
+  string = string
+    .replace(new RegExp(propToReplace, "g"), propValue);
+  return string;
+};
 
-  // Remove the class 'active' from home and switch to Menu button
-  var switchMenuToActive = function () {
-    // Remove 'active' from home button
-    var classes = document.querySelector("#navHomeButton").className;
-    classes = classes.replace(new RegExp("active", "g"), "");
-    document.querySelector("#navHomeButton").className = classes;
+// Remove the class 'active' from home and switch to Menu button
+var switchMenuToActive = function () {
+  // Remove 'active' from home button
+  var classes = document.querySelector("#navHomeButton").className;
+  classes = classes.replace(new RegExp("active", "g"), "");
+  document.querySelector("#navHomeButton").className = classes;
 
-    // Add 'active' to menu button if not already there
-    classes = document.querySelector("#navMenuButton").className;
-    if (classes.indexOf("active") === -1) {
-      classes += " active";
-      document.querySelector("#navMenuButton").className = classes;
-    }
-  };
+  // Add 'active' to menu button if not already there
+  classes = document.querySelector("#navMenuButton").className;
+  if (classes.indexOf("active") === -1) {
+    classes += " active";
+    document.querySelector("#navMenuButton").className = classes;
+  }
+};
 
   // On page load (before images or CSS)
 document.addEventListener("DOMContentLoaded", function (event) {
